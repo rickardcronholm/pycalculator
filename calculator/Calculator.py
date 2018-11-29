1b3e5d5aec802d6b65251ca445caa70054d7087a
Rickard Cronholm <rickard.cronholm@spectronic.se>
2018-11-29 14:51:17 +0100

    Implemeted Basic functionality

---
+++ calculator/Calculator.py
@@ -0,0 +1,16 @@
+class Calculator():
+
+    def __init__(self):
+        pass
+
+    def add(self, x, y):
+        return x + y
+
+    def minus(self, x, y):
+        return x - y
+
+    def multiple(self, x, y):
+        return x * y
+
+    def devide(self, x, y):
+        return x/y
